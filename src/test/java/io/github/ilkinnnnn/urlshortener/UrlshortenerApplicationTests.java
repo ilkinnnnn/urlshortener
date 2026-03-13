e03575159c18b1b3eefe92d5a36718c6dd27a62c
ilkin <ilkinismayilzad@gmail.com>
2026-03-13 15:26:47 +0400

    Initial commit

---
+++ src/test/java/io/github/ilkinnnnn/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ilkinnnnn.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
